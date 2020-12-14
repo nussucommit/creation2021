e248366c86d256d8349b203997eca527db0c8508
Kevin Mingtarja <kevin.mingtarja@u.nus.edu>
2020-12-15 03:34:53 +0800

    Refined Submission Feature

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.submit, name="submit")
-    # Kyknya register/ gaperlu di add, krn udah ada path dari main app (creation)
+    path("", views.submit, name="submit"),
+    path("1/", views.form_1, name='1'),
+    path("2/", views.form_2, name='2'),
+    path("3/", views.form_3, name='3')
 ]
