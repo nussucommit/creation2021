a73f3ade83e4b0269ec71aebc0c33f3e331a6ca2
Nielsen <nielsencugito@gmail.com>
2020-12-17 14:28:02 +0800

    Refactored form view

--- users/urls.py
+++ users/urls.py
@@ -3,8 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.submit, name="submit"),
-    path("1/", views.form_1, name='1'),
-    path("2/", views.form_2, name='2'),
-    path("3/", views.form_3, name='3')
+    path("submit/", views.submit, name="submit"),
+    path("submit/<int:pk>/", views.form, name='submitdetail'),
 ]
