0efa8d9f8b68a18f61202eb39e3595b8258f7bf9
Nielsencu <50238554+Nielsencu@users.noreply.github.com>
2021-01-18 16:39:35 +0800

    changed title's logo

--- creation2021/settings.py
+++ creation2021/settings.py
@@ -133,6 +133,9 @@ USE_TZ = True
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
 
